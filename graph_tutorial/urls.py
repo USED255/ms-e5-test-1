e9ceeef0c2ce9f305d957b74f7d2dfbf212cc7f3
我如天翼 <wrtyis@outlook.com>
2022-10-25 15:52:45 +0800

    一键格式化

--- graph_tutorial/urls.py
+++ graph_tutorial/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 from tutorial import views
 
 urlpatterns = [
-    path('', include('tutorial.urls')),
-    path('admin/', admin.site.urls),
+    path("", include("tutorial.urls")),
+    path("admin/", admin.site.urls),
 ]
 # </UrlConfSnippet>
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -6,12 +6,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  # /
-  path('', views.home, name='home'),
-  # TEMPORARY
-  path('signin', views.sign_in, name='signin'),
-  path('signout', views.sign_out, name='signout'),
-  path('calendar', views.calendar, name='calendar'),
-  path('callback', views.callback, name='callback'),
-  path('calendar/new', views.newevent, name='newevent'),
+    # /
+    path("", views.home, name="home"),
+    # TEMPORARY
+    path("signin", views.sign_in, name="signin"),
+    path("signout", views.sign_out, name="signout"),
+    path("calendar", views.calendar, name="calendar"),
+    path("callback", views.callback, name="callback"),
+    path("calendar/new", views.newevent, name="newevent"),
 ]
